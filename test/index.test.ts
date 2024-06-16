ba8ee3dc25471b49c1c0cd93201c54f390076b4d
heisenye <Heisenye@163.com>
2024-06-16 12:05:40 +0800

    test: add initial test directory and test function

---
+++ test/index.test.ts
@@ -0,0 +1,38 @@
+import { describe, it, expect } from "vitest"
+import { config } from "../src/index.js"
+import { Linter } from "eslint"
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended"
+import typescriptParser from "@typescript-eslint/parser"
+
+describe("function config", () => {
+  it("should merge user config with default config", () => {
+    const userConfig: Linter.FlatConfig = {
+      files: ["**/*.vue"],
+      rules: {
+        "no-unuesd-vars": "error",
+      },
+    }
+
+    const expectedConfig = [
+      {
+        files: ["**/*.{js,ts}", "**/*.vue"],
+        languageOptions: {
+          parser: typescriptParser,
+        },
+        rules: {
+          "no-unuesd-vars": "error",
+          "prettier/prettier": [
+            "error",
+            {
+              semi: false,
+            },
+          ],
+        },
+      } satisfies Linter.FlatConfig,
+      eslintPluginPrettier,
+    ]
+
+    const result = config(userConfig)
+    expect(result).toEqual(expectedConfig)
+  })
+})
