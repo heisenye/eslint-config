11a126cbd3fb1b52a245f363b63670a91cc77965
heisenye <heisenye@163.com>
2024-07-18 22:21:03 +0800

    feat: add some options in config function

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import { Linter } from "eslint"
 import { javascript, typescript, prettier, node } from "./configs"
-import { GLOB_DIST } from "./globs"
+import { GLOB_ALL_JS, GLOB_DIST, GLOB_TS } from "./globs"
 
 interface ConfigOptions {
   globalIgnores?: string[]
   userConfig?: Linter.FlatConfig
+  useBundle?: boolean
+  ifPublish?: boolean
 }
 
 export function config(options: ConfigOptions = {}) {
   const configs: Linter.FlatConfig[] = []
-  const { userConfig } = options
+  const { userConfig, useBundle, ifPublish } = options
 
   // default ignores are listed here
   // {
@@ -26,6 +28,26 @@ export function config(options: ConfigOptions = {}) {
 
   if (userConfig) {
     configs.push(userConfig)
+  }
+
+  if (useBundle) {
+    configs.push({
+      files: [GLOB_TS, GLOB_ALL_JS],
+      rules: {
+        "n/no-missing-import": "off",
+        "n/no-missing-require": "off",
+      },
+    })
+  }
+
+  if (!ifPublish) {
+    configs.push({
+      files: [GLOB_TS, GLOB_ALL_JS],
+      rules: {
+        "n/no-unpublished-import": "off",
+        "n/no-unpublished-require": "off",
+      },
+    })
   }
 
   return configs
