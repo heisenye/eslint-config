dcfff94f2ae01e05cce866cd9a70622cb8d7aa57
heisenye <heisenye@163.com>
2024-07-18 22:13:53 +0800

    chore: update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,7 @@
 import { config } from "./dist/index.mjs"
 
 const eslintConfig = config({
-  userConfig: {
-    files: ["**/*.?(c|m)js", "**/*.ts"],
-    rules: {
-      "n/no-missing-import": "off",
-      "n/no-missing-require": "off",
-    },
-  },
+  useBundle: true
 })
 
 export default eslintConfig
