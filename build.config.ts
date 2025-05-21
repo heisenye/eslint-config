0c08d00a4af8c3c7f18597fc3ddecf0469a9469e
heisenye <heisenye@163.com>
2025-05-21 20:35:11 +0800

    chore(build): add externals to unbuild config

--- build.config.ts
+++ build.config.ts
@@ -1,6 +1,13 @@
 import { defineBuildConfig } from "unbuild"
 
 export default defineBuildConfig({
+  externals: [
+    "eslint",
+    "@eslint/js",
+    "eslint-plugin-n",
+    "@typescript-eslint/parser",
+    "@typescript-eslint/utils",
+  ],
   clean: true,
   entries: ["src/index"],
   rollup: {
