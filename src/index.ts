9fad35eb7edec22f72e3a8614aad372223b53a95
heisenye <heisenye@163.com>
2025-07-09 14:40:00 +0800

    chore(core): adjust 'javascript', 'typescirpt' parameters and add tsconfigRootDir parameter for createConfig

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Linter } from "eslint"
 import { javascript, typescript, prettier, node, preset } from "./configs"
 import { DEFAULT_IGNORES } from "./utils"
 import type { ConfigOptions } from "./utils"
+import { fileURLToPath } from "node:url"
+import { dirname } from "node:path"
 
 export function createConfig({
   ecmaVersion = "latest",
@@ -10,6 +12,7 @@ export function createConfig({
   javascriptRules = {},
   typescriptRules = {},
   typeChecking = false,
+  tsconfigRootDir = fileURLToPath(dirname(import.meta.url)),
   presetOptions = {},
   globalIgnores = [],
   userConfigs = [],
@@ -22,8 +25,14 @@ export function createConfig({
   })
 
   configs.push(
-    javascript({ ecmaVersion, jsx, rules: javascriptRules }),
-    ...typescript({ ecmaVersion, jsx, rules: typescriptRules, typeChecking }),
+    javascript({ ecmaVersion, jsx, javascriptRules }),
+    ...typescript({
+      ecmaVersion,
+      jsx,
+      typescriptRules,
+      typeChecking,
+      tsconfigRootDir,
+    }),
     ...prettier(),
   )
 
