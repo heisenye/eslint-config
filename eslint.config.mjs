21d3a97e4b63819dceab05f06630d062f6dedf1b
heisenye <heisenye@163.com>
2025-07-09 14:42:47 +0800

    chore(eslint): update configuration for latest setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,14 @@
 import { createConfig } from "./dist/index.mjs"
 
 const eslintConfig = createConfig({
-  jsx: true,
   environment: "node",
   presetOptions: {
     allowMissingModules: true,
     allowUnpublishedModules: true,
+    allowExtraneousModules: true,
   },
+  typeChecking: true,
+  tsconfigRootDir: "tsconfig.json",
 })
 
 export default eslintConfig
